4ac1af771eb8b3f37190120cf25892558fac57e8
Okan <businesss187@gmail.com>
2018-06-28 16:36:15 +0200

    test

--- app/src/main/java/com/example/jannikokan/timetable/Table/HomeActivity.java
+++ app/src/main/java/com/example/jannikokan/timetable/Table/HomeActivity.java
@@ -28,6 +28,7 @@ public class HomeActivity extends AppCompatActivity {
 
 //test
     //okan
+    // okan 2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
